1c6acde517c5320a68052302a529692eacee6209
Mohamed Heikel BOUZAYENE <mhbouzayenne@dom.tti>
2023-08-08 16:55:53 +0100

    test lambda function in aws

--- Exercice1/Exercice1.py
+++ Exercice1/Exercice1.py
@@ -151,7 +151,7 @@ REAC_REF_ACT_TYPE_SCHEMA: StructType = StructType(
 
 if __name__ == "__main__":
     
-    print("test Lambda11")
+    print("test Lambda12")
     spark = (SparkSession.builder \
         .appName("Exercice 1") \
         .getOrCreate())
