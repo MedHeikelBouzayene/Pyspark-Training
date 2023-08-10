14baec38641f2b73b0f795f9dd9ba7afbe7cbe22
Mohamed Heikel BOUZAYENE <mhbouzayenne@dom.tti>
2023-08-10 14:01:29 +0100

    Correct File Names

--- Exercice2/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from pyspark.sql import SparkSession
-
-@pytest.fixture(scope="session")
-def spark():
-    spark = SparkSession.builder.appName("pytest-pyspark").getOrCreate()
-    yield spark
-    spark.stop()
