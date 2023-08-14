20f0d2414d83832015a689e463de64bf39c06223
Mohamed Heikel BOUZAYENE <mhbouzayenne@dom.tti>
2023-08-14 23:09:57 +0100

    test jenkins

--- Exercise1/Exercice1-glue.py
+++ Exercise1/Exercice1-glue.py
@@ -149,7 +149,7 @@ REAC_REF_ACT_TYPE_SCHEMA: StructType = StructType(
 
 if __name__ == "__main__":
     
-    print("test jenkins14")
+    print("test jenkins15")
     spark = (SparkSession.builder \
         .appName("Exercice 1-Read-Write-From-S3") \
         .getOrCreate())
